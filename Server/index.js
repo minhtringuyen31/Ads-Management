bf016517841d99cb7cd97fa20adc572398d23165
thien <79550249+thienhk15@users.noreply.github.com>
2023-12-03 14:43:08 +0700

    test

--- Server/index.js
+++ Server/index.js
@@ -1,20 +1,21 @@
-import cors from 'cors';
-import dotenv from 'dotenv';
-import express from 'express';
-import { errorHandler, notFound } from './helper/errorHandler.js';
-import db from './configs/db.js';
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import { errorHandler, notFound } from "./helper/errorHandler.js";
+import db from "./configs/db.js";
+import { Report } from "./models/ReportModel.js";
 
 dotenv.config();
 
 const app = express();
 const corsOptions = {
-    origin: 'http://localhost:' + process.env.PORT,
+  origin: "http://localhost:" + process.env.PORT,
 };
 
 const initializeExpress = (app) => {
-    app.use(cors(corsOptions));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
+  app.use(cors(corsOptions));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 };
 
 initializeExpress(app);
@@ -22,6 +23,6 @@ db();
 app.use(errorHandler);
 
 app.listen(process.env.PORT, () => {
-    console.log('Server is running on port: ' + process.env.PORT);
-    console.log('http://localhost:' + process.env.PORT);
+  console.log("Server is running on port: " + process.env.PORT);
+  console.log("http://localhost:" + process.env.PORT);
 });
