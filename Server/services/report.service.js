2f686b21b132c197b01a918bf4cb98e806c089f0
thien <79550249+thienhk15@users.noreply.github.com>
2023-12-17 15:13:54 +0700

    add report coordinate

--- Server/services/report.service.js
+++ Server/services/report.service.js
@@ -2,8 +2,28 @@ import { Report } from "../models/ReportModel.js";
 
 const ReportService = {
   async getAll(filter, projection) {
-      return await Report.find(filter).select(projection);
-  }  ,
+    try {
+      const reports = await Report.find({})
+        .populate({
+          path: "location",
+          model: "Location", // Replace with the actual name of the Location model
+        })
+        .populate({
+          path: "board",
+          model: "AdsBoard", // Replace with the actual name of the Board model
+          populate: {
+            path: "location_id",
+            model: "Location", // Replace with the actual name of the Location model
+          },
+        })
+        .exec();
+  
+      return reports;
+    } catch (error) {
+      console.error("Error getting reports:", error);
+      throw error;
+    }
+  },
   // async getAll() {
   //   return await Report.find();
   // },
