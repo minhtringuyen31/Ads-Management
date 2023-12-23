0f98a3511cad331a84ed4f9cba0ff979273ccc7c
Bui Quang Thanh <buiquangthanh1709@gmail.com>
2023-12-23 13:40:50 +0700

    push code

--- Server/utils/logger.js
+++ Server/utils/logger.js
@@ -1,16 +1,5 @@
 import winston from 'winston';
 import DailyRotateFile from 'winston-daily-rotate-file';
-import fs from 'fs';
-import path from 'path';
-
-// Đường dẫn đến thư mục log
-const logDirectory = path.join(__dirname, 'logs/api');
-
-// Kiểm tra xem thư mục log có tồn tại không
-if (!fs.existsSync(logDirectory)) {
-    fs.mkdirSync(logDirectory, { recursive: true }); // Tạo thư mục
-}
-
 const logger = winston.createLogger({
     level: 'info',
     format: winston.format.combine(
