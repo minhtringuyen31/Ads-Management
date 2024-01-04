7a91c667916ad79084b38021e3618e5493ae0918
Bui Quang Thanh <buiquangthanh1709@gmail.com>
2024-01-04 16:21:27 +0700

    upload code

--- Server/controllers/authorizeRequest.controller.js
+++ Server/controllers/authorizeRequest.controller.js
@@ -47,7 +47,7 @@ const AuthorizeRequestController = {
 
   create: async (req, res, next) => {
     try {
-      const reportData = { ...req.body };
+      const reportData = JSON.parse(JSON.stringify(req.body));
 
       const files = req.files;
       if (files) {
